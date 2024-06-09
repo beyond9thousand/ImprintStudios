915ed1907a4323b3c1956a723104d77411c11fc7
beyond9thousand <77363063+beyond9thousand@users.noreply.github.com>
2024-06-09 14:57:19 +0530

    Update VideoEditing_Pipeline.bat

--- Scripts/VideoEditing_Pipeline.bat
+++ Scripts/VideoEditing_Pipeline.bat
@@ -1,27 +1,40 @@
 @echo off
+setlocal enabledelayedexpansion
+
+:: Prompt for the project name
 set /p Project="Enter Project Name: "
 
+:: Retrieve and format the current date as DDMMYY
 for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /value') do set "dt=%%I"
-set "PDate=%dt:~6,2%%dt:~4,2%%dt:~2,2%"
+set "FormattedDate=%dt:~6,2%%dt:~4,2%%dt:~2,2%"
 
-md ".\%PDate%_%Project%\"
-cd ".\%PDate%_%Project%\"
+:: Define the root directory path for the project
+set "RootDir=.\%FormattedDate%_%Project%"
 
+:: Create the root directory
+md "%RootDir%"
+
+:: Define and create subdirectories
 for %%D in (
-   "01_ProjectFiles"
-   "02_References"
-   "03_Media\01_RawFootage"
-   "03_Media\02_Proxies"
-   "03_Media\03_Audio\01_Music"
-   "03_Media\03_Audio\02_VoiceOvers"
-   "03_Media\03_Audio\03_SFX"
-   "03_Media\04_Images"
-   "04_Assets\01_Graphics"
-   "04_Assets\02_Titles"
-   "04_Assets\03_VFX"
-   "05_Sequences"
-   "06_Exports\01_WorkInProgress"
-   "06_Exports\02_Final"
-   "07_Backups"
-   "08_Delivery"
-) do md "%%D"
+    "01_ProjectFiles",
+    "02_References",
+    "03_Media\01_RawFootage",
+    "03_Media\02_Proxies",
+    "03_Media\03_Audio\01_Music",
+    "03_Media\03_Audio\02_VoiceOvers",
+    "03_Media\03_Audio\03_SFX",
+    "03_Media\04_Images",
+    "04_Assets\01_Graphics",
+    "04_Assets\02_Titles",
+    "04_Assets\03_VFX",
+    "05_Sequences",
+    "06_Exports\01_WorkInProgress",
+    "06_Exports\02_Final",
+    "07_Backups",
+    "08_Delivery"
+) do md "%RootDir%\%%D"
+
+:: Optional: Change to the project root directory
+cd "%RootDir%"
+
+:: End of script
