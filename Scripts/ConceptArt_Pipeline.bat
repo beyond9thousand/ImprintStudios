edd7031d56d1789c370845dc62a35054f6fdab99
beyond9thousand <77363063+beyond9thousand@users.noreply.github.com>
2025-04-07 13:52:42 +0530

    Update ConceptArt_Pipeline.bat

--- Scripts/ConceptArt_Pipeline.bat
+++ Scripts/ConceptArt_Pipeline.bat
@@ -16,19 +16,26 @@ md "%RootDir%"
 
 :: Define and create subdirectories
 for %%D in (
-    "01_PreProduction",
-    "02_References",
-    "03_Storyboards\01_Thumbnails",
-    "03_Storyboards\02_Clean_Storyboards",
-    "04_ConceptArt\01_Characters",
-    "04_ConceptArt\02_Environments",
-    "04_ConceptArt\03_Props",
-    "05_Styleframes",
-    "06_Deliverables",
-    "07_Feedback\01_Internal",
-    "07_Feedback\02_Client",
-    "08_Backups"
+    "01_PreProduction\01_Briefings",
+    "01_PreProduction\02_Moodboards",
+    "01_PreProduction\03_Sketches",
+    "02_Assets\01_Source_Files\01_Raster_Graphics",
+    "02_Assets\01_Source_Files\02_Vector_Graphics",
+    "02_Assets\01_Source_Files\04_Other_Design_Files",
+    "02_Assets\02_References",
+    "02_Assets\03_Fonts",
+    "02_Assets\04_Stock_Images",
+    "02_Assets\05_Vectors",
+    "03_Design\01_Drafts",
+    "03_Design\02_Review_Files",
+    "03_Design\03_Final_Designs",
+    "04_Delivery\01_Client_Preview",
+    "04_Delivery\02_Final_Files",
+    "05_Backups\01_Project_Backups",
+    "05_Backups\02_Design_Backups"
 ) do md "%RootDir%\%%D"
 
+:: Optionally change to the project root directory
 cd "%RootDir%"
+
 :: End of script
